1a228dcd3cdfe4d09c68d824e39210ccf384a847
Brian Longmore <branodev@gmail.com>
2024-10-01 23:30:43 -0700

    worky

--- handlers/item.go
+++ handlers/item.go
@@ -19,59 +19,18 @@ func CreateItem(c *fiber.Ctx) error {
 
 	ownerId := c.Locals("jwt").(*jwt.Token).Claims.(jwt.MapClaims)["userId"].(string)
 
-	// create item with no category
-	// if category does not exist, create category with no items
-	// update item to have categoryName
-	// update category to append item
-
-	newItem, err := prisma.Client.Item.CreateOne(
+	_, err := prisma.Client.Item.CreateOne(
 		db.Item.Owner.Link(
 			db.User.ID.Equals(ownerId),
 		),
 		db.Item.Name.Set(fields.Name),
 		db.Item.Condition.Set(fields.Condition),
 		db.Item.Description.Set(fields.Description),
+		db.Item.Categories.Set(fields.Categories),
 	).Exec(prisma.Ctx)
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "Database could not create item",
-		})
-	}
-
-	newCategory, err := prisma.Client.Category.CreateOne(
-		db.Category.Name.Set(fields.CategoryName),
-		db.Category.Description.Set(fields.Description),
-	).Exec(prisma.Ctx)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			// "message": "Database could not create category",
-			"error": err.Error(),
-		})
-	}
-
-	_, err = prisma.Client.Item.FindUnique(
-		db.Item.ID.Equals(newItem.ID),
-	).Update(
-		db.Item.Category.Link(
-			db.Category.Name.Equals(fields.CategoryName),
-		),
-	).Exec(prisma.Ctx)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Database could not update item to include new category",
-		})
-	}
-
-	_, err = prisma.Client.Category.FindUnique(
-		db.Category.ID.Equals(newCategory.ID),
-	).With(
-		db.Category.Items.Fetch(),
-	).Update(
-		db.Category.Items.Push(newItem),
-	).Exec(prisma.Ctx)
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": "Database could not update category",
 		})
 	}
 
